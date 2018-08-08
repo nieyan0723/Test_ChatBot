0ce7bfaf690918231ba6665001fb074c60e5bf4b
nieyan0723 <nieyan0723@gmail.com>
2018-08-07 23:18:14 -0700

    remove token

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const WIT_TOKEN = process.env.WIT_TOKEN || 'YTNBAIGFVMM6775WFFKYP32JDE3BHCOZ'
+const WIT_TOKEN = process.env.WIT_TOKEN
 if (!WIT_TOKEN) {
   throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.')
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAGI9ncKQxgBAId2DD1fSohvkGmrBGCe2M3xD4SRdWhQ6YG99rpMgE2ZCA4pAMZBbUEKLr1ZCYo03qwQfAvxMouEy0dEiUTWVoq9THVUm1UuUpvQbo5Aa2aX2P7vyCX4HXlTZCeoNpqHecqfrTBUeBuUmJtuQ2NJXq1t3dMR2YgvyA7auZCg1'
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN 
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
